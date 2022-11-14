1d4d86ff8e6ebd9fe1e809eb64e553f2000ad0df
ahakobyan0809 <a.hakobyan0809@gmail.com>
2022-11-14 20:33:50 +0400

    Update scraper.js

--- src/scrapers/scraper.js
+++ src/scrapers/scraper.js
@@ -4,22 +4,6 @@ const constants = require('../constants')
 const Profiles = require("../models/linkedin-profiles");
 const { getNameAndLastName, getProfilePicture, sendConnectionRequest  } = require('../helpers/linkedin-helpers')
 
-// const FULL_NAME_SELECTOR = 'div.ph5.pb5 > div.mt2.relative > div:nth-child(1) > div:nth-child(1) > h1'
-// const IMAGE_SELECTOR = 'div.ph5 > div:nth-child(1) > div.pv-top-card--photo.text-align-left.pv-top-card--photo-resize > div > button img'
-// const CONNECT_SELECTOR = 'div.ph5 > div.pv-top-card-v2-ctas.display-flex.pt3 > div > div.pvs-profile-actions__action';
-//
-// const accounts = ['https://www.linkedin.com/in/rajiaabdelaziz',
-//     'https://www.linkedin.com/in/darian-bhathena/',
-//     'https://www.linkedin.com/in/danilolucari/',
-//     'https://www.linkedin.com/in/ngellner/',
-//     'https://www.linkedin.com/in/sixped/',
-//     'https://www.linkedin.com/in/davidezequielgranados/',
-//     'https://www.linkedin.com/in/andrejvajagic/',
-//     'https://www.linkedin.com/in/sahilbhatiya/',
-//     'https://www.linkedin.com/in/stenrs/',
-//     'https://www.linkedin.com/in/alexghattas/'
-// ];
-
 async function openBrowserAndSetCookies() {
     try {
         const browser = await puppeteer.launch(
